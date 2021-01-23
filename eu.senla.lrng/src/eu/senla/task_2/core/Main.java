c502b91c3ac0cdcc38986d342087604a8a9bcbdd
63110 <63110for@list.ru>
2021-01-23 13:58:27 +0300

    Update Main.java

--- eu.senla.lrng/src/eu/senla/task_2/core/Main.java
+++ eu.senla.lrng/src/eu/senla/task_2/core/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         Rainbow rainbow = new Rainbow();
         Scanner input = new Scanner(System.in);
+        System.out.println("Choose number or color rainbow 1-7");
         int colorNumber = input.nextInt();
         rainbow.getColor(colorNumber);
     }
