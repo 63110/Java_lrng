b774a5ba781fd076b0cf74b44511d1208a77929b
63110 <63110for@list.ru>
2021-01-24 15:19:59 +0300

    task3

---
+++ eu.senla.lrng/src/eu.senla.task_3/Cat.java
@@ -0,0 +1,59 @@
+package eu.senla.task_3;
+
+public class Cat {
+
+    public String  name= "dasdsa";
+    public  int  age = 1231;
+
+    public Cat (String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+
+    }
+
+
+
+
+
+    String catName = "feodor";
+    String catAge = "12y.o";
+
+
+    private String  catName1;
+    private int catAge1;
+
+    public String getCatName1() {
+        return catName1;
+    }
+
+    public void setCatName1(String catName1) {
+        this.catName1 = catName1;
+    }
+
+    public int getCatAge1() {
+        return catAge1;
+    }
+
+    public void setCatAge1(int catAge1) {
+        this.catAge1 = catAge1;
+    }
+
+    public void ts() {
+        int count = 0;
+
+        while (count < 10) {
+            count++;
+            System.out.println("Name:" + catName + " ");
+            System.out.println("Age:" + catAge);
+
+        }
+    }
+
+
+
+
+
+}
+
+
---
+++ eu.senla.lrng/src/eu.senla.task_3/core/Main.java
@@ -0,0 +1,51 @@
+package eu.senla.task_3.core;
+
+import eu.senla.task_3.Cat;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Cat cat = new Cat("misha", 12);
+
+
+        Scanner input = new Scanner(System.in);
+
+        //<<<<<<<<<<task1>>>>>>>>>>>
+     cat.ts();
+//<<<<<<<<<<task2>>>>>>>>>>>
+
+        cat.setCatName1("Mavrodui");
+        cat.setCatAge1(12);
+        String catName_1= cat.getCatName1();
+        int catAge_1= cat.getCatAge1();
+
+        for (int i = 0; i<10; i++){
+            System.out.println("Name:"+ catName_1);
+            System.out.println("Age:" + catAge_1);
+        }
+//<<<<<<<<<<<<<<task3>>>>>>>>>>>>>
+
+
+        int counter = 0;
+        do {counter++;
+            System.out.print("Name:"+ cat.name+ " ");
+            System.out.println("age:"+ cat.age);
+        } while (counter < 10);
+
+//<<<<<<<<<<<<<<task4>>>>>>>>>>>>>
+        int []array =new int [5];
+        cat.setCatName1("shaman");
+        cat.setCatAge1(4);
+        for (int ii: array ){
+            System.out.println("Name:"+ cat.getCatName1() + " ");
+            System.out.println("Age:"+ cat.getCatAge1());
+        }
+
+
+
+
+    }
+
+}
